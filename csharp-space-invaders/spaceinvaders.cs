1b33313283a478908c94a99628736e1130160de1
steve <stenox@gmx.de>
2020-06-06 17:16:11 +0200

    add spaceship

--- csharp-space-invaders/spaceinvaders.cs
+++ csharp-space-invaders/spaceinvaders.cs
@@ -16,6 +16,10 @@ namespace csharp_space_invaders
         // the timer that forces our window to refresh
         private static System.Windows.Forms.Timer refresh_timer;
 
+        private Image space_ship;
+
+        private Random rnd;
+
         private void timer_event(Object source, EventArgs e)
         {
             // refreshes the window, forcing it to re-draw (so OnPaint gets called again)
@@ -24,13 +28,32 @@ namespace csharp_space_invaders
 
         protected override void OnPaint(PaintEventArgs e)
         {
-            // windows forms onpaint aufrufen
+            // call windows forms onpaint
             base.OnPaint(e);
 
+            // our graphics object
             Graphics g = e.Graphics;
 
-            g.DrawString("Hallo Welt", DefaultFont, new SolidBrush(Color.White), 0, 0);
-            g.DrawString("Ihr seid alle blöd!", DefaultFont, new SolidBrush(Color.White), 0, DefaultFont.Size + 5);
+            // position of our space ship
+            int x = 100;
+            int y = 100;
+
+            // size of our space ship
+            int size_x = 100;
+            int size_y = 100;
+
+            // the size of our window
+            Size window_client_size = ClientSize;
+
+            // draw spaceship at random position
+            x = rnd.Next(0, window_client_size.Width - size_x);
+            y = rnd.Next(0, window_client_size.Height - size_y);
+
+            // create the rectangle (position and size) for DrawImage
+            RectangleF rect = new RectangleF(x, y, size_x, size_y);
+
+            // draw our space ship
+            g.DrawImage(space_ship, rect);
         }
 
         public spaceinvaders()
@@ -44,6 +67,12 @@ namespace csharp_space_invaders
             refresh_timer.Interval = (int)(1000.0 / ((double)target_fps)); // turn FPS to millisecond interval
             refresh_timer.Tick += new System.EventHandler(timer_event);
             refresh_timer.Start();
+
+            // load our space ship image
+            space_ship = Image.FromFile("ship.png");
+
+            // create random object
+            rnd = new Random();
         }
     }
 }
