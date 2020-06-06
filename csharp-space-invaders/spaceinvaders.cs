4d1be4c4def5a9d39bb6fdb68f88d54f74f44e23
steve <stenox@gmx.de>
2020-06-06 16:02:49 +0200

    basic rendering system

--- csharp-space-invaders/spaceinvaders.cs
+++ csharp-space-invaders/spaceinvaders.cs
@@ -12,9 +12,38 @@ namespace csharp_space_invaders
 {
     public partial class spaceinvaders : Form
     {
+        
+        // The timer that forces our window to refresh
+        private static System.Windows.Forms.Timer refresh_timer;
+
+        private void timer_event(Object source, EventArgs e)
+        {
+            // Refreshes the window, forcing it to re-draw (so OnPaint gets called again)
+            Refresh();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            Graphics g = e.Graphics;
+            using (Pen selPen = new Pen(Color.Blue))
+            {
+                using (Brush brush = new SolidBrush(Color.Blue)) g.FillRectangle(brush, 10, 10, 50, 50);
+            }
+        }
+
         public spaceinvaders()
         {
             InitializeComponent();
+
+            // Create the timer that keeps refreshing our window
+            int target_fps = 60; // How many FPS our window should have
+
+            refresh_timer = new System.Windows.Forms.Timer();
+            refresh_timer.Interval = (int)(1000.0 / ((double)target_fps)); // turn FPS to millisecond interval
+            refresh_timer.Tick += new System.EventHandler(timer_event);
+            refresh_timer.Start();
         }
     }
 }
